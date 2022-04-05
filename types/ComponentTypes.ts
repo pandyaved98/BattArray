b8eb386e82a635669bc72c31c006bc6264f2b663
Kartikey Vaish <55032587+kartikeyvaish@users.noreply.github.com>
2022-04-05 16:02:30 +0545

    Kartikey nearby maps component (#3)
    * :sparkles: created some constants
    
    * :sparkles: created some reusable components
    
    * :zap: created custom hooks
    
    * :zap: created types/interfaces for components
    
    * :package: installed `expo-location`
    
    * :sparkles: created a nearbuyvendors component

---
+++ types/ComponentTypes.ts
@@ -0,0 +1,42 @@
+// import the required packages
+import { ActivityIndicatorProps, ColorValue, StyleProp, TextProps, ViewStyle } from "react-native";
+
+// import local types 
+import { ChildrenProps, SeperateMarginProps } from "./GlobalTypes";
+import { ReactNativePaperButtonProps } from "./PaperTypes";
+
+// type for AppButton  
+export type AppButtonProps = {
+    title?: string;
+    children?: any;
+    height?: number;
+    roundness?: number;
+    backgroundColor?: string;
+    color?: string;
+    elevation?: number;
+} & Omit<ReactNativePaperButtonProps, 'children'> & ChildrenProps & SeperateMarginProps;
+
+// interface for AppLoading
+export interface AppLoadingProps {
+    visible?: boolean;
+    loadingText?: string;
+    loadingTextProps?: AppTextProps;
+    indicatorProps?: ActivityIndicatorProps;
+    containerStyles?: StyleProp<ViewStyle>;
+}
+
+// interface for AppText
+export interface AppTextProps extends TextProps, SeperateMarginProps {
+    text?: string;
+    color?: ColorValue;
+    size?: number;
+    family?: string;
+}
+
+// interface for BackDropProps
+export interface BackDropProps {
+    style?: any;
+    children?: any;
+    onBackDropPress?: any;
+    visible?: boolean;
+}
